7bc6120bd9f3485eebecefc594e98c0a805a6126
rennavittorio <renna.vittorio@gmail.com>
2023-02-03 14:53:41 +0100

    START file set uo + STYLE some style added

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Mail
+// Creaun array con una lista di email (stringhe)
+// Chiedi all’utente la sua email (con il prompt), controlla che sia nella lista di email,
+// stampa un messaggio appropriato sull’esito del controllo.
